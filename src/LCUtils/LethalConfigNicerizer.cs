87397702187eeb74daa4a6085c8c441a577b66e5
DecibillyJoel <23534814+DecibillyJoel@users.noreply.github.com>
2025-04-22 17:59:35 -0400

    Fix LethalConfigNicerizer making duplicate pages in Lethal Config

--- src/LCUtils/LethalConfigNicerizer.cs
+++ src/LCUtils/LethalConfigNicerizer.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using BepInEx.Bootstrap;
 using BepInEx.Configuration;
@@ -15,18 +16,18 @@ public static class LethalConfigNicerizer
     }
 
 	[MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
-	public static ConfigEntry<T> Nicerize<T>(ConfigEntry<T> entry, bool restartRequired = false)
+	public static ConfigEntry<T> Nicerize<T>(ConfigEntry<T> entry, bool restartRequired = false, Assembly? callingAssembly = null)
 	{
 		if (entry is ConfigEntry<int>) {
-			if (CanHasNicerizationPlease) AddConfigItem((entry as ConfigEntry<int>)!, restartRequired);
+			if (CanHasNicerizationPlease) AddConfigItem((entry as ConfigEntry<int>)!, restartRequired, callingAssembly ?? Assembly.GetCallingAssembly());
 		} else if (entry is ConfigEntry<float>) {
-			if (CanHasNicerizationPlease) AddConfigItem((entry as ConfigEntry<float>)!, restartRequired);
+			if (CanHasNicerizationPlease) AddConfigItem((entry as ConfigEntry<float>)!, restartRequired, callingAssembly ?? Assembly.GetCallingAssembly());
 		} else if (entry is ConfigEntry<bool>) {
-			if (CanHasNicerizationPlease) AddConfigItem((entry as ConfigEntry<bool>)!, restartRequired);
+			if (CanHasNicerizationPlease) AddConfigItem((entry as ConfigEntry<bool>)!, restartRequired, callingAssembly ?? Assembly.GetCallingAssembly());
 		} else if (entry is ConfigEntry<string>) {
-			if (CanHasNicerizationPlease) AddConfigItem((entry as ConfigEntry<string>)!, restartRequired);
+			if (CanHasNicerizationPlease) AddConfigItem((entry as ConfigEntry<string>)!, restartRequired, callingAssembly ?? Assembly.GetCallingAssembly());
 		} else if (entry is ConfigEntry<Enum>) {
-			if (CanHasNicerizationPlease) AddConfigItem((entry as ConfigEntry<Enum>)!, restartRequired);
+			if (CanHasNicerizationPlease) AddConfigItem((entry as ConfigEntry<Enum>)!, restartRequired, callingAssembly ?? Assembly.GetCallingAssembly());
 		} else {
 			throw new ArgumentException($"[LCUtils.LethalConfigNicerizer.Nicerize] Cannot Nicerize ConfigEntry<{typeof(T)}>!");
 		}
@@ -35,48 +36,48 @@ public static class LethalConfigNicerizer
 	}
 
 	[MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
-    public static void AddConfigItem(ConfigEntry<int> entry, bool restartRequired = false)
+    public static void AddConfigItem(ConfigEntry<int> entry, bool restartRequired = false, Assembly? callingAssembly = null)
     {
 		if(entry.Description.AcceptableValues != null) {
-			LethalConfigManager.AddConfigItem(new LethalConfig.ConfigItems.IntSliderConfigItem(entry, restartRequired));
+			LethalConfigManager.AddConfigItem(new LethalConfig.ConfigItems.IntSliderConfigItem(entry, restartRequired), callingAssembly);
 		} 
 		else {
-			LethalConfigManager.AddConfigItem(new LethalConfig.ConfigItems.IntInputFieldConfigItem(entry, restartRequired));
+			LethalConfigManager.AddConfigItem(new LethalConfig.ConfigItems.IntInputFieldConfigItem(entry, restartRequired), callingAssembly);
 		}
     }
 
 	[MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
-    public static void AddConfigItem(ConfigEntry<float> entry, bool restartRequired = false)
+    public static void AddConfigItem(ConfigEntry<float> entry, bool restartRequired = false, Assembly? callingAssembly = null)
     {
 		if(entry.Description.AcceptableValues != null) {
-			LethalConfigManager.AddConfigItem(new LethalConfig.ConfigItems.FloatSliderConfigItem(entry, restartRequired));
+			LethalConfigManager.AddConfigItem(new LethalConfig.ConfigItems.FloatSliderConfigItem(entry, restartRequired), callingAssembly);
 		} 
 		else {
-			LethalConfigManager.AddConfigItem(new LethalConfig.ConfigItems.FloatInputFieldConfigItem(entry, restartRequired));
+			LethalConfigManager.AddConfigItem(new LethalConfig.ConfigItems.FloatInputFieldConfigItem(entry, restartRequired), callingAssembly);
 		}
     }
 
 	[MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
-    public static void AddConfigItem(ConfigEntry<bool> entry, bool restartRequired = false)
+    public static void AddConfigItem(ConfigEntry<bool> entry, bool restartRequired = false, Assembly? callingAssembly = null)
     {
-		LethalConfigManager.AddConfigItem(new LethalConfig.ConfigItems.BoolCheckBoxConfigItem(entry, restartRequired));
+		LethalConfigManager.AddConfigItem(new LethalConfig.ConfigItems.BoolCheckBoxConfigItem(entry, restartRequired), callingAssembly);
     }
 
 	[MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
-    public static void AddConfigItem(ConfigEntry<string> entry, bool restartRequired = false)
+    public static void AddConfigItem(ConfigEntry<string> entry, bool restartRequired = false, Assembly? callingAssembly = null)
     {
 		if(entry.Description.AcceptableValues != null) {
-			LethalConfigManager.AddConfigItem(new LethalConfig.ConfigItems.TextDropDownConfigItem(entry, restartRequired));
+			LethalConfigManager.AddConfigItem(new LethalConfig.ConfigItems.TextDropDownConfigItem(entry, restartRequired), callingAssembly);
 		}
 		else
 		{
-			LethalConfigManager.AddConfigItem(new LethalConfig.ConfigItems.TextInputFieldConfigItem(entry, restartRequired));
+			LethalConfigManager.AddConfigItem(new LethalConfig.ConfigItems.TextInputFieldConfigItem(entry, restartRequired), callingAssembly);
 		}
     }
 
 	[MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
-    public static void AddConfigItem(ConfigEntry<Enum> entry, bool restartRequired = false)
+    public static void AddConfigItem(ConfigEntry<Enum> entry, bool restartRequired = false, Assembly? callingAssembly = null)
     {
-		LethalConfigManager.AddConfigItem(new LethalConfig.ConfigItems.EnumDropDownConfigItem<Enum>(entry, restartRequired));
+		LethalConfigManager.AddConfigItem(new LethalConfig.ConfigItems.EnumDropDownConfigItem<Enum>(entry, restartRequired), callingAssembly);
     }
 }
